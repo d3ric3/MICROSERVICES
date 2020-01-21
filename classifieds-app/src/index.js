3a31d6a23df726302940ba8ffc4054b77c0ec049
Derice <d3ric3@gmail.com>
2020-01-21 11:33:02 +0800

    part 7

--- classifieds-app/src/index.js
+++ classifieds-app/src/index.js
@@ -2,11 +2,13 @@ import "@babel/polyfill";
 import React from "react";
 import { ApolloProvider } from "react-apollo";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 import graphqlClient from "#root/api/graphqlClient";
 import Root from "#root/components/Root";
 
+import store from "./store";
 import * as theme from "./theme";
 
 const GlobalStyle = createGlobalStyle`
@@ -25,11 +27,13 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 render(
-  <ApolloProvider client={graphqlClient}>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Root />
-    </ThemeProvider>
-  </ApolloProvider>,
+  <Provider store={store}>
+    <ApolloProvider client={graphqlClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Root />
+      </ThemeProvider>
+    </ApolloProvider>
+  </Provider>,
   document.getElementById("app")
 );
